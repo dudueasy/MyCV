145584e0b0159577cc249e54d8248a7044b5c30b
dudueasy <dudueasy@126.com>
2018-03-06 02:34:14 +0800

    改善用户评论功能, 增加评论区样式

--- script/message.js
+++ script/message.js
@@ -30,35 +30,39 @@ form.addEventListener('submit', (e) => {
 // 表单提交事件处理函数
 function submitHandler(e) {
     e.preventDefault()
-    let commentData = document.querySelector('#comment-data').value
+    let commentData = form.querySelector('#comment-data').value
+    let username = form.querySelector('#comment-user-name').value
     let queryObj = AV.Object.extend(className)
     let obj = new queryObj()
 
-    createData(commentData, 'usercomment')
-    RetriveData(className, messageContainer)
-
+    createData(commentData, username, 'usercomment', messageContainer)
 }
 
 
 //-------------------- CRUD --------------------
 ////-----CREATE 写入数据, className 是数据表名.-----
-function createData(commentData, className) {
+function createData(commentData, username, className, messageContainer) {
 
     let queryObj = AV.Object.extend(className)
     let obj = new queryObj()
 
-    obj.save({data: commentData})
-        .then(
-            (response) => {
-                console.log('提交成功');
-                console.log(response);
-                RetriveData(className);
-
-            }
-            ,
-            () => {
-                console.log('提交失败')
-            })
+    obj.save({
+        data: commentData,
+        user: username
+    }).then(
+        (response) => {
+            console.log('提交成功');
+            console.log(response);
+            // 更新页面评论数据
+            let li = document.createElement('li')
+            li.innerText = `${response.attributes.user} : ${response.attributes.data}`
+            messageContainer.appendChild(li)
+            form.querySelector('#comment-data').value = ''
+        }
+        ,
+        () => {
+            console.log('提交失败')
+        })
 }
 
 ////-----Retrieve 批量获取数据.-----
@@ -71,18 +75,13 @@ function RetriveData(className, messageContainer) {
     query.find().then(function (objects) {
             dataList = []
 
-            // 每次展示数据前先清理原数据
-            while (messageContainer.firstChild) {
-                messageContainer.removeChild(messageContainer.firstChild)
-            }
-
             objects.map(item => {
-                dataList.push(item.attributes.data)
+                dataList.push(item.attributes)
             })
 
             dataList.forEach((value) => {
                 let li = document.createElement('li')
-                li.innerText = value
+                li.innerText = `${value.user} : ${value.data}`
                 messageContainer.appendChild(li)
             })
         }
