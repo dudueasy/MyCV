908bc6acdce9500545357d4b1f3cd12afb2d5538
dudueasy <dudueasy@126.com>
2018-03-06 03:58:18 +0800

    用mvc结构来整理评论功能代码

--- script/message.js
+++ script/message.js
@@ -1,93 +1,98 @@
 // 用于控制 leancloud 的脚本
+//// 立即执行!
+!function () {
+    let view = document.querySelector('section.message')
 
-// 初始化 AV 对象
-let APP_ID = 'u6evWlV1eKl06LPxfhhnrcbL-gzGzoHsz';
+    let model = {
+        init: function () {
+            // 初始化 leanCloud AV 对象
+            let APP_ID = 'u6evWlV1eKl06LPxfhhnrcbL-gzGzoHsz';
+            let APP_KEY = 'ryrfwoInyc4xw6Vuolkwoc7u';
+            AV.init({
+                appId: APP_ID,
+                appKey: APP_KEY
+            });
+        },
+        fetch: (dataTableName) => {
+            let query = new AV.Query(dataTableName);
+            return query.find()
+        },
+        save: (dataTableName, commentData, username) => {
+            let queryObj = AV.Object.extend(dataTableName)
+            let obj = new queryObj()
 
-let APP_KEY = 'ryrfwoInyc4xw6Vuolkwoc7u';
-
-AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-});
-
-
-// 初始化一个表单和查询的数据表名称
-let form = document.querySelector('#user-comments-form')
-let className = 'usercomment'
-
-// 初始化用户评论容器
-let messageContainer = document.querySelector('#message-list > ul')
-
-// 初始化评论数据
-RetriveData(className, messageContainer)
-
-// 给表单绑定提交事件监听器
-form.addEventListener('submit', (e) => {
-    submitHandler(e)
-})
-
-
-// 表单提交事件处理函数
-function submitHandler(e) {
-    e.preventDefault()
-    let commentData = form.querySelector('#comment-data').value
-    let username = form.querySelector('#comment-user-name').value
-    let queryObj = AV.Object.extend(className)
-    let obj = new queryObj()
-
-    createData(commentData, username, 'usercomment', messageContainer)
-}
-
-
-//-------------------- CRUD --------------------
-////-----CREATE 写入数据, className 是数据表名.-----
-function createData(commentData, username, className, messageContainer) {
-
-    let queryObj = AV.Object.extend(className)
-    let obj = new queryObj()
-
-    obj.save({
-        data: commentData,
-        user: username
-    }).then(
-        (response) => {
-            console.log('提交成功');
-            console.log(response);
-            // 更新页面评论数据
-            let li = document.createElement('li')
-            li.innerText = `${response.attributes.user} : ${response.attributes.data}`
-            messageContainer.appendChild(li)
-            form.querySelector('#comment-data').value = ''
-        }
-        ,
-        () => {
-            console.log('提交失败')
-        })
-}
-
-////-----Retrieve 批量获取数据.-----
-////// 使用 AV.QUERY API
-//////  参数分别为查询的 数据表名, 用于展示数据的 DOM 元素
-function RetriveData(className, messageContainer) {
-    // 定义查询的数据表
-    let query = new AV.Query(className);
-    let dataList
-    query.find().then(function (objects) {
-            dataList = []
-
-            objects.map(item => {
-                dataList.push(item.attributes)
-            })
-
-            dataList.forEach((value) => {
-                let li = document.createElement('li')
-                li.innerText = `${value.user} : ${value.data}`
-                messageContainer.appendChild(li)
+            return obj.save({
+                data: commentData,
+                user: username
             })
         }
-        ,
-        function (error) {
-            console.log('遇到异常', error)
-        });
-}
+    }
+    let controller = {
+        view: null,
+        model: null,
+        messageContainer: null,
+        dataTableName: null,
+        commentData: '',
+        username: '',
+        form: null,
+        init: function (view, model) {
+            this.view = view
+            this.model = model
+            this.model.init()
+            this.messageContainer = document.querySelector('#message-list > ul')
+            this.dataTableName = 'usercomment'
+            this.form = view.querySelector('#user-comments-form')
+            this.loadMessages(this.dataTableName, this.messageContainer)
+            this.bindEvents(this.form, this.submitHandler)
+        },
 
+        loadMessages: function (dataTableName, messageContainer) {
+
+            model.fetch(dataTableName).then((objects) => {
+                    let dataList = []
+
+                    objects.map(item => {
+                        dataList.push(item.attributes)
+                    })
+
+                    dataList.forEach((value) => {
+                        let li = document.createElement('li')
+                        li.innerText = `${value.user} : ${value.data}`
+                        messageContainer.appendChild(li)
+                    })
+                }
+                ,
+                function (error) {
+                    console.log('遇到异常', error)
+                });
+        },
+        submitHandler: function (e) {
+            this.commentData = this.form.querySelector('#comment-data').value
+            this.username = this.form.querySelector('#comment-user-name').value
+
+            model.save(this.dataTableName, this.commentData, this.username)
+                .then((response) => {
+                        console.log('提交成功');
+                        console.log(response);
+                        // 更新页面评论数据
+                        let li = document.createElement('li')
+                        li.innerText = `${response.attributes.user} : ${response.attributes.data}`
+                        this.messageContainer.appendChild(li)
+                        this.form.querySelector('#comment-data').value = ''
+                    }
+                    ,
+                    () => {
+                        console.log('提交失败')
+                    })
+        },
+        bindEvents: function () {
+            console.log(this)
+            this.form.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.submitHandler(e)
+            })
+        }
+    }
+
+    controller.init(view, model)
+}()
